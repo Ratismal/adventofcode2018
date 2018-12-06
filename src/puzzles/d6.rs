17fad77dc41ab6a10c7a0f046ea52667fb0c2661
stupid cat <kirkbenm@gmail.com>
2018-12-05 23:25:12 -0700

    day 6 part a will do part b tomorrow

--- src/puzzles/d6.rs
+++ src/puzzles/d6.rs
@@ -1,22 +1,156 @@
 use puzzles::puzzle::Puzzle;
+use std::collections::HashMap;
 
 pub struct Day {}
 
 impl Puzzle for Day {
   fn desc(&self) -> (String, String) {
     return (
-      String::from("To be determined..."),
+      String::from("Manhattan Distance"),
       String::from("To be determined..."),
     );
   }
 
   fn puzzle_a(&self, input: String) -> String {
-    let _lines = input.lines();
-    return String::from("Placeholder");
+    let mut points: HashMap<String, Point> = HashMap::new();
+    let mut max_point = Point {
+      x: 0,
+      y: 0,
+      valid: true,
+      area: 0,
+    };
+    for line in input.lines() {
+      let parts: Vec<&str> = line.split(", ").collect();
+      let mut point = Point {
+        x: parts[0].parse().unwrap(),
+        y: parts[1].parse().unwrap(),
+        valid: true,
+        area: 0,
+      };
+      if point.x > max_point.x {
+        max_point.x = point.x;
+      }
+      if point.y > max_point.y {
+        max_point.y = point.y;
+      }
+      points.insert(point.serialize(), point);
+    }
+    for x in 0..=max_point.x {
+      for y in 0..=max_point.y {
+        let mut point = Point {
+          x,
+          y,
+          valid: false,
+          area: 0,
+        };
+        let p = self.check_point(&point, &points, &max_point);
+        if points.contains_key(&p.serialize()) {
+          points.insert(p.serialize(), p);
+        }
+      }
+    }
+    let mut largest_area: i32 = 0;
+    let mut largest_point = Point {
+      x: 0,
+      y: 0,
+      area: 0,
+      valid: false,
+    };
+    for (_, point) in points {
+      println!("{}: {}, {}", point.serialize(), point.area, point.valid);
+      if point.valid && point.area > largest_area {
+        largest_area = point.area;
+        largest_point = point;
+      }
+    }
+    return largest_point.area.to_string();
   }
 
   fn puzzle_b(&self, input: String) -> String {
     let _lines = input.lines();
     return String::from("Placeholder");
+  }
+}
+
+impl Day {
+  fn check_point(&self, target: &Point, points: &HashMap<String, Point>, max: &Point) -> Point {
+    let mut matches: Vec<Point> = Vec::new();
+    // shrug
+    let mut smallest: i32 = 9999;
+    for (_, point) in points {
+      if target.x == point.x && target.y == point.y {
+        continue;
+      }
+      let distance = target.distance(&point);
+
+      if distance < smallest {
+        matches.clear();
+        smallest = distance;
+        // println!(
+        //   "*{}: {}, {} | {}",
+        //   point.serialize(),
+        //   point.area,
+        //   point.valid,
+        //   distance
+        // );
+
+        matches.push(point.clone());
+      } else if distance == smallest {
+        matches.push(point.clone());
+      }
+    }
+    // println!("============");
+    let mut point: Point;
+    if matches.len() == 1 {
+      point = matches[0].clone();
+      point.area += 1;
+    } else {
+      point = Point {
+        x: 0,
+        y: 0,
+        area: 0,
+        valid: false,
+      };
+    }
+
+    // println!(
+    //   "{}: {}, {} | {}",
+    //   matches[0].serialize(),
+    //   matches[0].area,
+    //   matches[0].valid,
+    //   matches.len()
+    // );
+
+    if target.x == 0 || target.y == 0 || target.x == max.x || target.y == max.y {
+      // println!("Marked invalid");
+      point.valid = false;
+    }
+    return point;
+  }
+}
+
+struct Point {
+  x: i32,
+  y: i32,
+  valid: bool,
+  area: i32,
+}
+
+impl Point {
+  fn distance(&self, target: &Point) -> i32 {
+    return (self.x - target.x).abs() + (self.y - target.y).abs();
+  }
+
+  fn serialize(&self) -> String {
+    return format!("{},{}", self.x, self.y);
+  }
+
+  fn clone(&self) -> Point {
+    Point {
+      x: self.x,
+      y: self.y,
+      valid: self.valid,
+      area: self.area,
+    }
   }
 }
