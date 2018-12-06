b4ffe7934fb762edff6b13c52a228ca980fd04fd
stupid cat <cat@blargbot.xyz>
2018-12-06 09:05:55 -0700

    finish day 6

--- src/puzzles/d6.rs
+++ src/puzzles/d6.rs
@@ -6,19 +6,74 @@ pub struct Day {}
 impl Puzzle for Day {
   fn desc(&self) -> (String, String) {
     return (
-      String::from("Manhattan Distance"),
-      String::from("To be determined..."),
+      String::from("Find furthest points"),
+      String::from("Find closest points"),
     );
   }
 
   fn puzzle_a(&self, input: String) -> String {
-    let mut points: HashMap<String, Point> = HashMap::new();
     let mut max_point = Point {
       x: 0,
       y: 0,
       valid: true,
       area: 0,
     };
+    let mut points = self.parse_points(&input, &mut max_point);
+
+    self.check_points(&mut points, &max_point);
+
+    let mut largest_area: i32 = 0;
+    let mut largest_point = Point {
+      x: 0,
+      y: 0,
+      area: 0,
+      valid: false,
+    };
+    for (_, point) in points {
+      // println!("{}: {}, {}", point.serialize(), point.area, point.valid);
+      if point.valid && point.area > largest_area {
+        largest_area = point.area;
+        largest_point = point;
+      }
+    }
+    return largest_point.area.to_string();
+  }
+
+  fn puzzle_b(&self, input: String) -> String {
+    let mut max_point = Point {
+      x: 0,
+      y: 0,
+      valid: true,
+      area: 0,
+    };
+    let points = self.parse_points(&input, &mut max_point);
+
+    let mut area_sum: i32 = 0;
+    for x in 0..=max_point.x {
+      for y in 0..=max_point.y {
+        let mut target = Point {
+          x,
+          y,
+          valid: false,
+          area: 0,
+        };
+        let mut distance_sum: i32 = 0;
+        for (_, point) in &points {
+          distance_sum += target.distance(&point);
+        }
+        if distance_sum < 10000 {
+          area_sum += 1;
+        }
+      }
+    }
+    return area_sum.to_string();
+  }
+}
+
+impl Day {
+  fn parse_points(&self, input: &String, max_point: &mut Point) -> HashMap<String, Point> {
+    let mut points: HashMap<String, Point> = HashMap::new();
+
     for line in input.lines() {
       let parts: Vec<&str> = line.split(", ").collect();
       let mut point = Point {
@@ -35,6 +90,11 @@ impl Puzzle for Day {
       }
       points.insert(point.serialize(), point);
     }
+
+    return points;
+  }
+
+  fn check_points(&self, points: &mut HashMap<String, Point>, max_point: &Point) {
     for x in 0..=max_point.x {
       for y in 0..=max_point.y {
         let mut point = Point {
@@ -49,30 +109,8 @@ impl Puzzle for Day {
         }
       }
     }
-    let mut largest_area: i32 = 0;
-    let mut largest_point = Point {
-      x: 0,
-      y: 0,
-      area: 0,
-      valid: false,
-    };
-    for (_, point) in points {
-      println!("{}: {}, {}", point.serialize(), point.area, point.valid);
-      if point.valid && point.area > largest_area {
-        largest_area = point.area;
-        largest_point = point;
-      }
-    }
-    return largest_point.area.to_string();
   }
 
-  fn puzzle_b(&self, input: String) -> String {
-    let _lines = input.lines();
-    return String::from("Placeholder");
-  }
-}
-
-impl Day {
   fn check_point(&self, target: &Point, points: &HashMap<String, Point>, max: &Point) -> Point {
     let mut matches: Vec<Point> = Vec::new();
     // shrug
